b503bd80b83a1718826960c7a6ea75c1a737ec02
YongWoonJi <woon013@naver.com>
2016-08-26 11:34:16 +0900

    08.25 - N

--- app/src/main/java/com/sender/team/sender/MainActivity.java
+++ app/src/main/java/com/sender/team/sender/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 //        supportRequestWindowFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
