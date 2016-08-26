26a38a68fc2c9f975cde06ab03ac22176ba72ba7
ChaPark <kureum12@naver.com>
2016-08-26 13:07:54 +0900

    08-26 Make Manager

---
+++ app/src/main/java/com/sender/team/sender/MyApplication.java
@@ -0,0 +1,23 @@
+package com.sender.team.sender;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by Administrator on 2016-08-09.
+ */
+public class MyApplication extends Application {
+    static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = this;
+//        FacebookSdk.sdkInitialize(getApplicationContext());
+//        AppEventsLogger.activateApp(this);
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}
