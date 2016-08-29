cd4e7bae315b02591bfbd2ecbedd3143187b0a9f
YongWoonJi <woon013@naver.com>
2016-08-29 15:18:12 +0900

    08.29 - add gcm

--- app/src/main/java/com/sender/team/sender/data/NetworkResult.java
+++ app/src/main/java/com/sender/team/sender/data/NetworkResult.java
@@ -1,12 +1,14 @@
 package com.sender.team.sender.data;
 
+import java.util.List;
+
 /**
  * Created by Tacademy on 2016-08-26.
  */
 public class NetworkResult<T> {
-    private T result;
+    private List<T> result;
 
-    public T getResult() {
+    public List<T> getResult() {
         return result;
     }
 }
