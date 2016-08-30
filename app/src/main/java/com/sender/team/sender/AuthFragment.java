a74144e75a2a90b912edd5cfa83c6519ca9b5fc2
YongWoonJi <woon013@naver.com>
2016-08-30 13:32:43 +0900

    08.30 - phone request

--- app/src/main/java/com/sender/team/sender/AuthFragment.java
+++ app/src/main/java/com/sender/team/sender/AuthFragment.java
@@ -1,20 +1,38 @@
 package com.sender.team.sender;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.SystemClock;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.sender.team.sender.data.NetworkResult;
 import com.sender.team.sender.manager.NetworkManager;
 import com.sender.team.sender.manager.NetworkRequest;
 import com.sender.team.sender.request.AddPhoneRequest;
+
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 /**
@@ -22,19 +40,61 @@ import com.sender.team.sender.request.AddPhoneRequest;
  */
 public class AuthFragment extends Fragment {
 
+    @BindView(R.id.edit_phone)
+    EditText editPhone;
+
+    @BindView(R.id.edit_authNum)
+    EditText editAuth;
+
+    @BindView(R.id.text_time)
+    TextView textTime;
+
 
     public AuthFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_auth, container, false);
+        ButterKnife.bind(this, view);
 
-        Button btn = (Button) view.findViewById(R.id.btn_finish);
+        checkPermission();
+
+        Button btn = (Button) view.findViewById(R.id.btn_getNum);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random random = new Random();
+                int num = random.nextInt(9000) + 1000;
+                editAuth.setText("" + num);
+
+                mHandler.post(runnable);
+            }
+        });
+
+
+        btn = (Button) view.findViewById(R.id.btn_ok);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setMessage("인증되었습니다");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+
+        btn = (Button) view.findViewById(R.id.btn_finish);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -66,4 +126,98 @@ public class AuthFragment extends Fragment {
 
         return view;
     }
+
+    private void checkPermission() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_PHONE_STATE)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Permission");
+                builder.setMessage("전화번호를 얻어옵니다");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        requestPermission();
+                    }
+                });
+                builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialogInterface) {
+
+                    }
+                });
+                builder.create().show();
+                return;
+            }
+            requestPermission();
+        } else {
+            Context context = getContext();
+            TelephonyManager manager = (TelephonyManager) context.getSystemService(context.TELEPHONY_SERVICE);
+            String phoneNum = manager.getLine1Number();
+            editPhone.setText(phoneNum);
+        }
+    }
+
+
+    private static final int RC_PERMISSION = 100;
+    private void requestPermission() {
+        requestPermissions(new String[] {Manifest.permission.READ_PHONE_STATE}, RC_PERMISSION);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == RC_PERMISSION) {
+            if (grantResults != null && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Context context = getContext();
+                TelephonyManager manager = (TelephonyManager) context.getSystemService(context.TELEPHONY_SERVICE);
+                String phoneNum = manager.getLine1Number();
+                editPhone.setText(phoneNum);
+            }
+        }
+    }
+
+    int minute = 3;
+    long startTime = -1;
+    Handler mHandler = new Handler(Looper.getMainLooper());
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            long time = SystemClock.elapsedRealtime();
+            if (startTime == -1) {
+                startTime = time;
+            }
+            int gap = (int) (time - startTime);
+            int second = 60 - gap / 1000;
+            int rest = 1000 - (gap % 1000);
+            if (second >= 0) {
+                if (second == 60) {
+                    textTime.setText(minute + " : 00");
+                    minute--;
+                } else {
+                    if (second < 10) {
+                        textTime.setText(minute + " : 0" + second);
+                        if (second == 0) {
+                            startTime = -1;
+                            minute--;
+                        }
+                    } else {
+                        textTime.setText(minute + " : " + second);
+                    }
+                }
+                mHandler.postDelayed(this, rest);
+            } else {
+//                if (minute == 0) {
+//                    return;
+//                }
+//                startTime = -1;
+//                minute--;
+//                second = 59;
+//                textTime.setText(minute + " : " + second);
+//                mHandler.postDelayed(this, rest);
+            }
+        }
+    };
+
 }
