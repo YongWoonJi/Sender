625a32aa14dffb1d3ae263a20d759cbc41cb3406
Eunji <kwakeunjea@naver.com>
2016-09-03 15:57:52 +0900

    09.03 - facebookRequest

---
+++ app/src/main/java/com/sender/team/sender/request/FacebookRequest.java
@@ -0,0 +1,50 @@
+package com.sender.team.sender.request;
+
+import android.content.Context;
+
+import com.google.gson.reflect.TypeToken;
+import com.sender.team.sender.data.NetworkResult;
+import com.sender.team.sender.manager.NetworkRequest;
+
+import java.lang.reflect.Type;
+
+import okhttp3.FormBody;
+import okhttp3.HttpUrl;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+/**
+ * Created by Tacademy on 2016-09-03.
+ */
+public class FacebookRequest extends AbstractRequest<NetworkRequest<String>> {
+
+    Request request;
+
+    public FacebookRequest(Context context, String access_token, String registration_token) {
+        HttpUrl url = getSecureUrlBuilder()
+                .addPathSegment("auth")
+                .addPathSegment("facebook")
+                .addPathSegment("token")
+                .build();
+        RequestBody body = new FormBody.Builder()
+                .add("access_token", access_token)
+                .add("registration_token", registration_token)
+                .build();
+
+        request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .tag(context)
+                .build();
+    }
+
+    @Override
+    protected Type getType() {
+        return new TypeToken<NetworkResult<String>>(){}.getType();
+    }
+
+    @Override
+    public Request getRequest() {
+        return request;
+    }
+}
